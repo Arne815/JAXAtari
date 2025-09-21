dabbf9e8a479859e5d32e989238fb62ac7d7d935
Arne815 <arne.98.lich@gmx.de>
2025-09-21 10:47:02 +0200

    test

--- src/jaxatari/games/jax_othello.py
+++ src/jaxatari/games/jax_othello.py
@@ -2806,9 +2806,9 @@ class JaxOthello(JaxEnvironment[OthelloState, OthelloObservation, OthelloInfo, O
         return jnp.concatenate([
             obs.player_score.flatten(),
             obs.enemy_score.flatten(),
-            obs.field.field_id,
-            obs.field.field_color,
-            obs.field_choice_player,
+            obs.field.field_id.flatten(),
+            obs.field.field_color.flatten(),
+            obs.field_choice_player.flatten(),
         ]
         )
     
